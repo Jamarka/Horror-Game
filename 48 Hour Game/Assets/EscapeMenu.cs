68930dc2d0cbbb48360479684490b2cd7f602377
Jamarka <jamarka@yahoo.com.au>
2015-12-10 22:59:08 +1000

    Menu Complete
    Just need to pretty it up

---
+++ 48 Hour Game/Assets/EscapeMenu.cs
@@ -0,0 +1,38 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+
+public class EscapeMenu : MonoBehaviour {
+
+	public Canvas escapeMenu;
+	public Button cont;
+	
+	//public PlayerMovement mouseLook;
+
+	// Use this for initialization
+	void Start () {
+		escapeMenu = escapeMenu.GetComponent<Canvas> ();
+		cont = cont.GetComponent<Button> ();
+		escapeMenu.enabled = false;
+		
+	}
+	
+	void Update() {
+			if (Input.GetKeyDown(KeyCode.Escape)){
+				escapeMenu.enabled = true;
+			//EscPress ();
+		}
+	}
+	
+	
+	public void Continue() {
+		escapeMenu.enabled = false;
+	}
+	//public void EscPress() {
+		//Time.timeScale = 0f;
+		//GetComponent("MouseLook").enabled = !GetComponent("MouseLook").enabled;
+		//escapeMenu.enabled = true;
+	
+		
+	
+}
---
+++ 48 Hour Game/Assets/_Script/MusicPlayer.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class MusicPlayer : MonoBehaviour {
+
+	public static MusicPlayer instance = null;
+
+	void Awake() {
+		print ("hello world");
+		if (instance != null) {
+			Destroy (gameObject);
+			print ("Duplicate music player self-destructing");
+		} else {
+			instance = this;
+			GameObject.DontDestroyOnLoad (gameObject);
+			print (instance);
+		}
+	}
+
+}
