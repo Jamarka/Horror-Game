0fcbde5b05935acd0c54d5f299b4b7093fb3359e
Jamarka <jamarka@yahoo.com.au>
2015-12-10 20:06:05 +1000

    New Version

---
+++ 48 Hour Game/Assets/_Script/LevelManager.cs
@@ -0,0 +1,30 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class LevelManager : MonoBehaviour {
+
+
+	// Use this for initialization
+	void Start () 
+	{
+
+	}
+	
+	// Update is called once per frame
+	void Update () 
+	{
+	
+	}
+	
+	void OnTriggerEnter(Collider target)
+	{
+		if (target.tag == "Player"){
+			print (target.tag);
+
+			Application.LoadLevel(0);
+		}
+		
+		
+		
+	}
+}
---
+++ 48 Hour Game/Assets/_Script/Traps/SpikeWallDetection.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class SpikeWallDetection : MonoBehaviour {
+
+	public MovingWall movingWall;
+	public bool hasHit = false;
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+	void OnCollisionEnter (Collision col)
+	{
+		if (col.collider.CompareTag("SpikedWall"))
+		{
+			movingWall.hasHitWall = true;
+			hasHit = true;
+		}
+	}
+}
