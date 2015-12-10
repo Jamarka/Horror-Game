31213fd3bcd602356ae48bc731a515adb331b6b7
Jamarka <jamarka@yahoo.com.au>
2015-12-10 11:48:08 +1000

    added sound
    sound is cool i think

--- 48 Hour Game/Assets/_Script/menuScript.cs
+++ 48 Hour Game/Assets/_Script/menuScript.cs
@@ -5,16 +5,12 @@ using System.Collections;
 public class menuScript : MonoBehaviour {
 
 	public Canvas quitMenu;
-	//public Button startText;
-	//public Button exitText;
 	public Canvas buttons;
 
 	// Use this for initialization
 	void Start () {
 	
 		quitMenu = quitMenu.GetComponent<Canvas> ();
-		//startText = startText.GetComponent<Button> ();
-		//exitText = exitText.GetComponent<Button> ();
 		buttons = buttons.GetComponent<Canvas> ();
 		quitMenu.enabled = false;
 	}
@@ -23,15 +19,11 @@ public class menuScript : MonoBehaviour {
 	public void ExitPress(){
 		quitMenu.enabled = true;
 		buttons.enabled = false;
-		//startText.enabled = false;
-		//exitText.enabled = false;
 	}
 	
 	public void NoPress() {
 		quitMenu.enabled = false;
 		buttons.enabled = true;
-		//startText.enabled = true;
-		//exitText.enabled = true;
 	}
 	
 	public void StartLevel() {
